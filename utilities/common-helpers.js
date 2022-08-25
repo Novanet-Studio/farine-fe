6426684c43c5624e5135a613426e2a2bc11ebbc3
Enrique Sarmiento <jeisonsarmiento@correo.unimet.edu.ve>
2022-08-25 12:30:14 -0400

    header sticky bug solved

--- utilities/common-helpers.js
+++ utilities/common-helpers.js
@@ -1,15 +1,16 @@
 export const stickyHeader = () => {
-    let number =
-        window.pageXOffset ||
-        document.documentElement.scrollTop ||
-        document.body.scrollTop ||
-        0;
-    const header = document.getElementById('headerSticky');
-    if (header !== null) {
-        if (number >= 300) {
-            header.classList.add('header--sticky');
-        } else {
-            header.classList.remove('header--sticky');
-        }
-    }
+	// console.log("la url?? ", window.location);
+	let number =
+		window.pageXOffset ||
+		document.documentElement.scrollTop ||
+		document.body.scrollTop ||
+		0;
+	const header = document.getElementById("headerSticky");
+	if (header !== null) {
+		if (number >= 100) {
+			header.classList.add("header--sticky");
+		} else {
+			header.classList.remove("header--sticky");
+		}
+	}
 };
