d34009533b7641dfcf7a55f2287e1464a882f927
David Arenas <davejs136@gmail.com>
2023-05-22 11:46:54 -0400

    style: set color and spacing for terms and conditions text

--- components/pago-movil.vue
+++ components/pago-movil.vue
@@ -91,9 +91,9 @@
       </div>
       <p>
         Al realizar esta compra usted acepta
-        <a href="#" class="text-yellow-400">nuestros términos y condiciones</a>.
+        <a href="#" class="text-color-2">nuestros términos y condiciones</a>.
       </p>
-      <div class="form__btn-group">
+      <div class="form__btn-group mt-4">
         <app-button
           btn-type="submit"
           text="Enviar"
