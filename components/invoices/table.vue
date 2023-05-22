63582bc72d43311006a27b8d29cf180d07d63d50
David Arenas <davejs136@gmail.com>
2023-05-22 12:22:14 -0400

    fix(style): invoice table responsive

--- components/invoices/table.vue
+++ components/invoices/table.vue
@@ -1,6 +1,8 @@
 <template>
   <!-- component -->
-  <div class="flex flex-col">
+  <div
+    class="flex flex-col whitespace-nowrap w-22rem max-w-22rem md:(w-full max-w-full)"
+  >
     <div class="overflow-x-auto sm:mx-0.5 lg:mx-0.5">
       <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
         <div class="overflow-hidden relative">
